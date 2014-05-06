6ef1d72c635b8b154dabba2d8d03b630cf1eba71
Ryan Griffin <ryan@therg.net>
2014-05-06 10:34:13 -0700

    scripts under functions so they are importable in python, reqs

--- extract_industry_mentions.py
+++ extract_industry_mentions.py
@@ -5,58 +5,64 @@ from nltk.probability import *
 from nltk.corpus import stopwords
 from nltk.util import bigrams, ngrams
 
-'''
-Set up vars
-'''
-reports = {}
-count = 0
-matches = []
+def extract_industry_mentions(text):
 
-'''
-Set up some industry lovin'
-'''
-industries = ['education', 'auto', 'b2b', 'beauty', 'beer', 'wine', 'spirits', 'kids', 'entertainment', 'fashion', 'financial', 'fine art', 'food beverage', 'gaming', 'government', 'healthcare', 'health wellness', 'home garden', 'hospitality', 'luxury', 'media', 'multicultural', 'music', 'news', 'non-profit', 'pets', 'pharmaceutical', 'real estate', 'retail', 'small business', 'sports', 'sustainable-green', 'tech', 'mobile', 'travel', 'tourism', 'publishing', 'startup']
+	'''
+	Set up vars
+	'''
+	reports = {}
+	count = 0
+	matches = []
 
-'''
-grab the text input:
-'''
-text = sys.argv[1]
-text = text.replace('\n', ' ')
-text = text.lower().strip()
+	'''
+	Set up some industry lovin'
+	'''
+	industries = ['education', 'auto', 'b2b', 'beauty', 'beer', 'wine', 'spirits', 'kids', 'entertainment', 'fashion', 'financial', 'fine art', 'food beverage', 'gaming', 'government', 'healthcare', 'health wellness', 'home garden', 'hospitality', 'luxury', 'media', 'multicultural', 'music', 'news', 'non-profit', 'pets', 'pharmaceutical', 'real estate', 'retail', 'small business', 'sports', 'sustainable-green', 'tech', 'mobile', 'travel', 'tourism', 'publishing', 'startup']
 
-'''
-tokenize and bigram
-'''
-tk_text = functions._word_tokenize(text)
-bigrams_all = nltk.bigrams(tk_text)
+	'''
+	grab the text input:
+	'''
+	text = text.replace('\n', ' ')
+	text = text.lower().strip()
 
-'''
-Find Industry Mentions in Bigrams
-'''
-for bigram_dict in bigrams_all:
-	bigram = bigram_dict[0] + ' ' + bigram_dict[1]
-	if bigram in industries:
-		matches.append(bigram)
-		count += 1
-		#remove to keep single word counts clean:
-		text = text.replace(bigram, '')
+	'''
+	tokenize and bigram
+	'''
+	tk_text = functions._word_tokenize(text)
+	bigrams_all = nltk.bigrams(tk_text)
 
-'''
-Find Industry Mentions in remaining words:
-'''
-words = text.split()
-for word in words:
-	if word in industries:
-		matches.append(word)
-		count += 1
+	'''
+	Find Industry Mentions in Bigrams
+	'''
+	for bigram_dict in bigrams_all:
+		bigram = bigram_dict[0] + ' ' + bigram_dict[1]
+		if bigram in industries:
+			matches.append(bigram)
+			count += 1
+			#remove to keep single word counts clean:
+			text = text.replace(bigram, '')
 
-'''
-Assemble the final output:
-'''
-for match in matches:
-	if match in reports:
-		reports[match] += 1
-	else:
-		reports[match] = 1
+	'''
+	Find Industry Mentions in remaining words:
+	'''
+	words = text.split()
+	for word in words:
+		if word in industries:
+			matches.append(word)
+			count += 1
 
-print json.dumps(reports)
+	'''
+	Assemble the final output:
+	'''
+	for match in matches:
+		if match in reports:
+			reports[match] += 1
+		else:
+			reports[match] = 1
+
+	return json.dumps(reports)
+
+
+if __name__ == '__main__':
+	mentions = extract_industry_mentions(sys.argv[1])
+	print mentions
